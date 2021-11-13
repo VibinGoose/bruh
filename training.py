2975304d16d7d1cd161d8b4638d8af113970d7ae
VibinGoose <88668914+VibinGoose@users.noreply.github.com>
2021-11-13 22:02:02 +0000

    Update training.py

--- training.py
+++ training.py
@@ -1,1 +1,25 @@
+import random, json, pickle, nltk
+import numpy as np
 
+from nltk.stem import WordNetLemmatizer as wnl     
+from tensorflow.keras.models import Sequential as seq
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD as sgd
+
+
+lem = wnl
+intents = json.loads(open("intents.json").read())
+words,classes,docs = []
+ignored_letters = ["?","!",".",","]
+
+
+for intent in intents["intents"]:
+    for pattern in intent["patterns"]:
+        word_list = nltk.tokenize(pattern)
+        words.append(word_list)
+        docs.append((word_list),intent["tag"])
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+
+
+print(docs)
